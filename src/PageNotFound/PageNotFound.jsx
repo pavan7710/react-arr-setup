561f109f786c1afac90f664c3f9c870eb9987f31
pavan-gloify <pavan@gloify.com>
2023-11-30 02:14:10 +0530

    module.scss

--- src/PageNotFound/PageNotFound.jsx
+++ src/PageNotFound/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import pagenotfound from '../assests/Logo/pagenotfound.png'
 import { Grid  , Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
-import './pagenotfound.scss'
+import  styles from './pagenotfound.module.scss'
 
 
 
@@ -14,17 +14,17 @@ const PageNotFound = () => {
 
   return (
     <React.Fragment>
-        <Grid className='bg-color' sx={{
+        <Grid className={styles.bgcolor} sx={{
             height :'100vh'
         }} container justifyContent="center" alignItems="center" direction="column">
-            <Grid  className='img-bottom' item sx={12} >
-                <img className='img-fluid' src={pagenotfound} alt='page_not_found' />
+            <Grid  className={styles.imgbottom} item sx={12} >
+                <img className={styles.imgfluid} src={pagenotfound} alt='page_not_found' />
             </Grid>
 
             <Grid item sx={12}>
                 <p>
-                    <span className='error_text'>ERROR.</span>
-                    <span className='page_not_found'>Page not found</span>
+                    <span className={styles.errortext}>ERROR.</span>
+                    <span className={styles.pagenotfound}>Page not found</span>
                 </p>
             </Grid>
 
