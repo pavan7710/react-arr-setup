eacf348e41addeef35bb6a1f864de1b225b48396
pavan-gloify <pavan@gloify.com>
2023-12-06 18:00:15 +0530

    data-grid implemented

--- src/modules/common/dashboard/Dashboard.jsx
+++ src/modules/common/dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import Inviteuser from "./components/Inviteuser";
 import { Box } from '@mui/material'
 import DashboardHeader from './components/Dashboardheader/DashboardHeader'
 import FilterButtons from './components/FilterButtons/Filterbuttons'
+import UserList from './components/UserData/UserList'
 
 const Dashboard = () => {
    return (
@@ -12,7 +13,14 @@ const Dashboard = () => {
             }}>
                 <DashboardHeader/>
             </Box>
-            <FilterButtons/>
+
+            <Box sx={{
+                mb : 8
+            }}>
+                <FilterButtons/>
+            </Box>
+
+            <UserList/>
     </React.Fragment>
    )
 };
---
+++ src/modules/common/dashboard/components/UserData/UserList.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import DataTable from 'src/Components/Datagrid'
+
+const UserList = () => {
+
+    const CustomNoRowsOverlay = () => {
+        return (
+            <p>No Data Found</p>
+        )
+      }
+
+    const columns = [
+        { field: '#', headerName: '#' , width : 100 },
+        { field: 'id', headerName: 'ID' , width : 150 },
+        { field: 'user_name', headerName: 'User Name' ,width : 150 },
+        { field: 'email', headerName: 'Email ID' ,  width : 250   },
+        { field: 'contact', headerName: 'Contact' ,width : 250 },
+        { field: 'role', headerName: 'Role', width : 250  },
+      ];
+      const rows =  [
+        {
+            "#" : "1",
+            "id" : "2",
+            "user_name" : "pavan",
+            "email" : "pavan@gmail.com",
+            "contact" : "9700657710",
+            "role" : "admin"
+
+        },
+        {
+            "#" : "1",
+            "id" : "2",
+            "user_name" : "pavan",
+            "email" : "pavan@gmail.com",
+            "contact" : "9700657710",
+            "role" : "admin"
+
+        },
+        {
+            "#" : "1",
+            "id" : "2",
+            "user_name" : "pavan",
+            "email" : "pavan@gmail.com",
+            "contact" : "9700657710",
+            "role" : "admin"
+
+        }
+      ]
+  return (
+    <React.Fragment>
+         <DataTable checkboxSelection disableColumnFilter disableColumnMenu
+        hideFooterPagination slots={{
+          noRowsOverlay: CustomNoRowsOverlay,
+        }}
+        autoHeight  rows={rows} columns={columns} disableRowSelectionOnClick />
+    </React.Fragment>    
+  )
+}
+
+export default UserList
